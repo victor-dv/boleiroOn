56e24675af44c3b32732a8028ea939c8cff4cd8c
victor <js813417@gmail.com>
2026-03-27 16:58:43 -0300

    git fit - Adicionando melhorias

--- src/main/java/br/com/boleiroOn/domain/lote/repository/LoteRepository.java
+++ src/main/java/br/com/boleiroOn/domain/lote/repository/LoteRepository.java
@@ -3,6 +3,10 @@ package br.com.boleiroOn.domain.lote.repository;
 import br.com.boleiroOn.domain.lote.entity.LoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoteRepository extends JpaRepository<LoteEntity, Long> {
     boolean existsByLeilaoIdAndNumeroLote(Long leilaoId, Integer numeroLote);
+
+    Optional<LoteEntity> findByLeilaoIdAndNumeroLote(Long leilaoId, Integer numeroLote);
 }
